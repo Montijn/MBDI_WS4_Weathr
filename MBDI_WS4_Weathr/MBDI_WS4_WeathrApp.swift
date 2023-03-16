dfe7728d4dbb29fbdcd2e6f5739ffaa42e2e5b97
Montijn <ms.bennik@student.avans.nl>
2023-03-16 10:42:02 +0100

    Initial Commit

---
+++ MBDI_WS4_Weathr/MBDI_WS4_WeathrApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBDI_WS4_WeathrApp.swift
+//  MBDI_WS4_Weathr
+//
+//  Created by Montijn Bennik on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MBDI_WS4_WeathrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
